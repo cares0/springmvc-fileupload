759539fdec0e52eb17ce58b7a30c47901ee639c3
cares0 <cares00000@gmail.com>
2022-05-20 13:04:25 +0900

    파일 업로드 - 서블릿

---
+++ src/main/java/hello/upload/controller/ServletUploadControllerV2.java
@@ -0,0 +1,76 @@
+package hello.upload.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+
+@Slf4j
+@Controller
+@RequestMapping("/servlet/v2")
+public class ServletUploadControllerV2 {
+
+    @Value("${file.dir}")
+    private String fileDir;
+
+    @GetMapping("/upload")
+    public String newFile() {
+
+        return "upload-form";
+    }
+
+    @PostMapping("/upload")
+    public String saveFileV2(HttpServletRequest request) throws ServletException, IOException {
+        log.info("request={}", request);
+
+        String itemName = request.getParameter("itemName");
+        log.info("itemName={}", itemName);
+
+        Collection<Part> parts = request.getParts();
+        log.info("parts={}", parts);
+
+        for (Part part : parts) {
+            log.info("==== PART ====");
+            log.info("name={}", part.getName());
+
+            Collection<String> headerNames = part.getHeaderNames();
+            for (String headerName : headerNames) {
+                log.info("header {}: {}", headerName, part.getHeader(headerName));
+            }
+
+            // 편의 메서드
+            // 헤더의 content-disposion 속성에는 파일명이 들어감
+            // 하지만, 파일명 뿐만 아니라 다른 값도 들어가서 일일이 파싱해야함
+            // 그걸 편리하게 해주는 편의 메서드 제공
+            log.info("submittedFileName={}", part.getSubmittedFileName());
+            log.info("size={}", part.getSize()); // 파트의 바디 사이즈
+
+            // 데이터 직접 읽기
+            InputStream inputStream = part.getInputStream();
+            String body = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+            log.info("body={}", body);
+
+            // 파일에 저장하기
+            if (StringUtils.hasText(part.getSubmittedFileName())) {
+                String fullPath = fileDir + part.getSubmittedFileName();
+                log.info("파일 저장 fullPath={}", fullPath);
+                part.write(fullPath);
+            }
+
+        }
+
+        return "upload-form";
+    }
+}
